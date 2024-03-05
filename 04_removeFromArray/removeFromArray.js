cb927db8d39a76710505097e46687326ced58246
Denis Le <dl.denisle@gmail.com>
2024-03-05 11:42:37 -0500

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...index) {
+    let newArray = [];
+    for (let element of array) {
+        if (!index.includes(element)) {
+            newArray.push(element);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
